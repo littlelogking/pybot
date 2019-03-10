c7b69b398d589ec10dae4803165f6adc1ea41863
littlelogking <louisg0709@gmail.com>
2019-03-10 18:06:29 +0000

    Started working on better simulation

---
+++ robot_simulation.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 17:47:27 2019
+
+@author: suilven
+"""
+#This Program is a better version of Robot_Image
+
+#Sim props:Height Width Speed Radius
+#Robot
+#see robot_image.py
+
+simprops={}
+robprops={}
+winprops={}
+
+def move_robot(event):
+    
+    #move the robot
+    
+def initialisewindow():
+    
+     #this is a list 
+    return windowprops
+
+def initialissim():
+    
+    
+    #this is a list    
+    return simprops
+
+
+def initialiserobot():
+    
+    
+    #this is a list
+    return robprops
+
+
+
+
+    
+c.bind_all('<Key>',move_robot)
+window.mainloop(0)
